982a64bfc7b0020304f12ba80cc3a462242e5778
0-don <don.cryptus@gmail.com>
2025-09-27 22:24:13 +0200

    Remove unused database operations and clean up grid initialization in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import "@dotenvx/dotenvx/config";
-import { sql } from "drizzle-orm";
-import { db } from "./db";
 import { COUNTRY_CODE } from "./lib/constants";
 import { generateCountryGrid } from "./lib/hex-grid-generator";
 
-await db.execute(sql`DELETE FROM grid_cell`);
 await generateCountryGrid(COUNTRY_CODE);
-// await gridManager.clearGrid();
-// await gridManager.initializeCountryGrid();
-// await gridManager.showLevelStats();
