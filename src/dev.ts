c49c9194fab54bdfbe9c120715795a3d22b4c388
0-don <don.cryptus@gmail.com>
2025-09-30 23:58:45 +0200

    Uncomment generator.split call and comment out the processing loop in dev.ts

--- src/dev.ts
+++ src/dev.ts
@@ -10,13 +10,13 @@ console.log(settings);
 const processor = new CellProcessor(settings);
 const generator = new GridGenerator(settings);
 
-// await generator.split(1);
+await generator.split(1);
 
-while (true) {
-  const result = await processor.processNext();
-  console.log(result);
-  if (!result) break;
-  if (result.needsSplit) await generator.split(result.cellId);
-}
+// while (true) {
+//   const result = await processor.processNext();
+//   console.log(result);
+//   if (!result) break;
+//   if (result.needsSplit) await generator.split(result.cellId);
+// }
 
 console.log("Processing complete!");
