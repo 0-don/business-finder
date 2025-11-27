b7ff5adf1687f68f675be3d99eb68713df4afaba
0-don <don.cryptus@gmail.com>
2025-11-27 02:41:18 +0100

    Remove GOOGLE_PLACES_API from environment variables in docker-compose and update type definitions in env.d.ts

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,10 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     DATABASE_URL: string;
-    GOOGLE_PLACES_API: string;
+
     GOOGLE_MAPS_JAVASCRIPT_API: string;
 
     DEFAULT_COUNTRY_CODE?: import(".").CountryCode;
-    DEFAULT_PLACE_TYPE?: import(".").PlaceType;
+    DEFAULT_PLACE_TYPE?: string;
   }
 }
